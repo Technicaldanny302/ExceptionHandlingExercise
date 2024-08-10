5ec665fbacd49134ad6b02a849311b351b5e4d06
Dan Marino <technicaldanny302@gmail.com>
2024-08-09 18:38:46 -0700

    completed exercise

--- ExceptionHandlingExercise/Program.cs
+++ ExceptionHandlingExercise/Program.cs
@@ -14,7 +14,33 @@ namespace ExceptionHandlingExercise
             // Use a try-catch statement to avoid a run-time exception.
             // Below we will set this up.
             // ------------------------------------------------------------------------------
+            var arr = new char[] { '1', '2', '3', '4', '5', '6', 'a', 'b', 'c' };
+            var numbers = new List<int>();
+            var str = "";
 
+            foreach (var item in arr)
+            {
+                int number;
+                try
+                {
+                    str = item.ToString();
+                    number = int.Parse(str);
+
+                    numbers.Add(number);
+                }catch (Exception e)
+                {
+                    Console.WriteLine($"UNABLE TO PARSE '{item}'");
+                }
+                finally
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+            foreach (var num in numbers)
+            {
+                Console.WriteLine(num);
+            }
 
 
             //TODO START HERE:
